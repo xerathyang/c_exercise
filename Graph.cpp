a27831e00445674307e8181a709d69cc6ad32315
TzuShianYang <c4940578@gmail.com>
2019-11-18 16:54:01 +0800

    graph(unfinished), heapsort

---
+++ Graph.cpp
@@ -0,0 +1,142 @@
+﻿// Graph.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int minDist(int** narray, int start, int vertices, int dist[], bool sSet[]) {
+	int minIndex = 0;
+	//int Indexcomp = 999;
+	//for (int i = 0; i < vertices; i++) {
+	//	if (Indexcomp > narray[start-1][i] && sSet[i] != 1 && narray[start-1][i] != 0) {
+	//		minIndex = i;
+	//		Indexcomp = narray[start-1][i];
+	//	}		
+	//}
+	int min = 999;
+	//cout << "set ";
+	//for (int i = 0; i < start; i++)
+	//	cout << sSet[i] << " ";
+	//cout << endl;
+	//cout << "dist ";
+	//for (int i = 0; i < start; i++)
+	//	cout <<dist[i] << " ";
+	//cout << endl;
+	for (int i = 0; i < vertices; i++) {
+		if (sSet[i] == false && dist[i] < min) {
+			min = dist[i];
+			minIndex = i;
+		}
+	}
+	return minIndex;
+}
+
+void path(int parent[], int a) {
+	if (parent[a] == -1)
+		return;
+	path(parent, parent[a]);
+	cout << a+1 << " ";
+}
+
+class Graph {
+public:
+	Graph(int, int, int);
+	void insert(int**,int, int, int);
+	void ptr(int**);
+private:
+	int node,start;
+};
+
+Graph::Graph(int n, int e, int s) {
+	node = n;
+	start = s;
+}
+
+void Graph::insert(int** narray,int p, int q, int d) {
+	
+	narray[p - 1][q - 1] = d;
+	narray[q - 1][p - 1] = d;
+}
+
+
+
+void Graph::ptr(int** narray) {
+	int* dist = new int[node];
+	bool* sSet = new bool[node];
+	int* parent = new int[node];
+
+	for (int i = 0; i < node; i++) {
+		parent[i] = -1;
+		dist[i] = 999;
+		sSet[i] = false;
+	}
+
+	dist[start-1] = 0;
+
+	for (int j = 0; j < node - 1; j++) {
+		int count = minDist(narray, start, node, dist, sSet);
+		sSet[count] = true;
+
+		//cout << "set ";
+		//for (int i = 0; i < start; i++)
+		//	cout << sSet[i] << " ";
+		//cout << endl;
+
+		for (int t = 0; t < node; t++) {
+
+			cout << narray[j][t] << endl;
+
+			if (sSet[t] == false && narray[j][t] && dist[j] + narray[j][t] < dist[t]) {
+				parent[t] = j;
+				//for (int i = 0; i < start; i++)
+				//	cout << parent[i] << " ";
+				//cout << endl;
+				dist[t] = dist[j] + narray[j][t];
+				//for (int i = 0; i < start; i++)
+				//	cout << dist[i] << " ";
+				//cout << endl;
+			}
+		}
+	}
+
+	//cout << "	parent	dist			sSet" << endl;
+	//for (int i = 0; i < node; i++) {
+	//	cout << i << "\t" << parent[i] << "\t" << dist[i] << "\t\t\t" << sSet[i] << endl;
+	//}
+
+	for (int i = 0; i < node; i++) {
+		if (i+1 == start) {
+			continue;
+		}
+		cout << "Distance of " << start << " to " << i+1 << " : " << dist[i] << endl;
+		cout << "Path: " << start << " ";
+		path(parent, i);
+		cout << endl;
+	}
+}
+
+int main()
+{
+	int input,n,e,s,p,q,d;
+	while (cin >> input) {
+		n = input;
+		int **narray = new int *[n];
+		for (int i = 0; i < n; i++){
+			narray[i] = new int[n];
+		}
+		for (int i = 0; i < n;i++){
+			for (int j = 0; j < n;j++){
+				narray[i][j] = 0;
+			}
+		}
+		cin >> e >> s;
+		Graph* shortetpath = new Graph(n, e, s);
+		for (int i = 0; i < e; i++) {
+			cin >> p >> q >> d;
+			shortetpath->insert(narray,p, q, d);
+		}
+		shortetpath->ptr(narray);
+	}
+}
---
+++ HeapSort.cpp
@@ -0,0 +1,95 @@
+﻿// HeapSort.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void swap(int& a, int& b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void modifty(vector<int> &v,int root,int length) {
+	int left = root * 2, right = root * 2 + 1, largest;
+	if (left <= length && v[left] > v[root])
+		largest = left;
+	else {
+		largest = root;
+	}
+	if (right <= length && v[right] > v[root]) {
+		if (v[right] > v[left] || largest == root) {
+			largest = right;
+		}
+	}
+	if (largest != root) {
+		swap(v[largest], v[root]);
+		modifty(v, largest, length);
+	}
+}
+
+void build(vector<int> &v) {
+	//調整內容
+	for (int i = v.size() / 2; i >= 1; i--) {
+		modifty(v,i,v.size()-1);
+	}
+}
+
+void heap(vector<int>& v) {
+	v.insert(v.begin(), 0);
+	build(v);
+	v.erase(v.begin());
+}
+
+void pop(vector<int> &v,int n) {
+	swap(v[0], v[n - 1]);
+	v.erase(v.begin() + n-1);
+	heap(v);
+}
+
+int main()
+{
+	int input=10,curr=1;
+	string ins="";
+	cin >> input;
+	int* arr = new int[input];
+	for (int i = 0; i < input; i++) {
+		cin >> arr[i];
+	}
+	vector<int> v(arr, arr + input);
+	heap(v);
+	//for (int i = 0; i < input; i++) {
+	//	cout << v[i] << " ";
+	//}
+	//cout << endl;
+	while (cin >> ins) {
+		if (ins == "show")
+			cout << v[curr-1] << endl;
+		else if (ins == "left") {
+			curr *= 2;
+			cout << v[curr-1] << endl;
+		}
+		else if (ins == "right") {
+			curr = curr * 2 + 1;
+			cout << v[curr - 1] << endl;
+		}
+		else if (ins == "heap") {
+			pop(v,input);
+			curr = 1;
+			input--;
+		}
+	}
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
